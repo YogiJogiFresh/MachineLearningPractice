5b3b47f950f14b595f1cac8d31d3f52e0a262bd9
Toshiwawa <wei.akai@gmail.com>
2017-07-24 18:53:11 -0400

    Apples and Oranges

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,13 @@
-import sklearn
+#First Machine Learning Program
+#How to identify apples and oranges given the Training data with Features:
+#Weight and Texture
+from sklearn import tree
+#0 = smooth
+#1 = bumpy
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+#0 = apple
+#1 = orange
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print clf.predict([[160, 0]])
